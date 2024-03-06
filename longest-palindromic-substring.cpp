fd752fd83ab5fcfb0200ee0c73aa6e5a6fdb1dd4
Alexander Töpfer <74869642+alexandertoepfer@users.noreply.github.com>
2024-03-06 11:24:25 +0100

    Create longest-palindromic-substring.cpp

---
+++ longest-palindromic-substring.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        // Brute force in O(n^3)
+        // 1. Create all possible substrings in a matrix
+        // 2. Expand around the center for each to verify
+        // TODO: Implementation
+    }
+};
